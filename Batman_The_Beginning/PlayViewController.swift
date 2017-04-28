010bdd7acfff6ff1fa74b966c52226befa255c55
Sagar Mangulkar <smangul1@binghamton.edu>
2017-04-28 03:58:36 -0400

    Implement jump of Batman by button

--- Batman_The_Beginning/PlayViewController.swift
+++ Batman_The_Beginning/PlayViewController.swift
@@ -11,6 +11,9 @@ import UIKit
 class PlayViewController: UIViewController {
 
     @IBOutlet var imageGifBatmanRunning: UIImageView!
+    @IBOutlet var buttonJump: UIButton!
+    @IBOutlet var imageBackground: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         imageGifBatmanRunning.loadGif(name: "batman_run")
@@ -22,15 +25,8 @@ class PlayViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func pushButtonJump(_ sender: Any) {
+        imageGifBatmanRunning.loadGif(name: "batman_jump")
     }
-    */
 
 }
